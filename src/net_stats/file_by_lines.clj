4177b385176d0ea4a7de90ae0980946c753e4bed
Arkadii Drovosekov <arkdro@protonmail.com>
2022-05-29 14:48:21 +0200

    print result line by line

--- src/net_stats/file_by_lines.clj
+++ src/net_stats/file_by_lines.clj
@@ -8,11 +8,33 @@
   [line]
   (= line item_util/separating_line))
 
+(defn is_everything_valid?
+  [items]
+  (every? some? items))
+
+(defn calc_and_print_delta
+  [timestamp interface rx1 tx1 rx2 tx2]
+  (let [rx_delta (max 0 (- rx2 rx1))
+        tx_delta (max 0 (- tx2 tx1))]
+    (println (.format item_util/formatter timestamp) interface rx1 tx1 rx_delta tx_delta)))
+
+(defn print_chunk_delta
+  [interface prev_data_chunk new_data_chunk]
+  (let [timestamp1 (:ts prev_data_chunk)
+        rx1 (get-in prev_data_chunk [:data :rx])
+        tx1 (get-in prev_data_chunk [:data :tx])
+        rx2 (get-in new_data_chunk [:data :rx])
+        tx2 (get-in new_data_chunk [:data :tx])]
+    (if (is_everything_valid? [timestamp1 rx1 tx1 rx2 tx2])
+      (calc_and_print_delta timestamp1 interface rx1 tx1 rx2 tx2)
+      nil ;; (.println *err* (str "invalid chunks, prev: " prev_data_chunk ", new: " new_data_chunk))
+      )))
+
 (defn process_text_chunk
   [chunk interface prev_data_chunk]
-  (let [data_chunk (item_util/get_data_from_chunk chunk interface)
-        ]
-    ))
+  (let [new_data_chunk (item_util/get_data_from_chunk chunk interface)]
+    (print_chunk_delta interface prev_data_chunk new_data_chunk)
+    new_data_chunk))
 
 (defn process_item
   [chunk interface prev_data_chunk]
